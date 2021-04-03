1adab167c4a265789728f8db68dd9dc4692430ec
Dimson-hub <81766226+Dimson-hub@users.noreply.github.com>
2021-04-03 16:24:33 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./client/index.html", 
+  filename: "./index.html"
+});
+
+module.exports = (env, argv) => {
+  console.log(argv.mode);
+  return {
+    entry: "./client/index.jsx",
+    output: { // NEW
+      path: path.join(__dirname, 'dist'),
+      filename: "[name].js"
+    }, // NEW Ends
+    
+    plugins: [htmlPlugin],
+    module: {
+      rules: [
+        
+
+
+        {
+          
+
+          test: /\.(js|jsx)$/, 
+          test: /\.(png|svg|jpg|gif)$/,
+
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          loader:"file-loader",
+          options: { name: '/static/[name].[ext]' }
+        }
+        
+        
+        }
+        
+        
+        
+        
+      ]
+      
+
+
+      
+        
+      
+    }
+
+  };
+};
